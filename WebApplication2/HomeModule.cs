fd3cb3d483fdee43e751a1fa3b1c94b7b1515650
Jonathan Channon <jonathan.channon@gmail.com>
2014-08-04 16:29:27 +0100

    initial commit

---
+++ WebApplication2/HomeModule.cs
@@ -0,0 +1,46 @@
+﻿using Nancy;
+using Nancy.Metadata.Module;
+using Nancy.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello";
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+            : base("/test")
+        {
+            Get["Test","/"] = _ => "Test";
+        }
+    }
+}
+
+//Make this namespace WebApplication2 and you'll see the Metadata
+namespace WebApplication2.Test
+{
+    public class TestMetadataModule : MetadataModule<SwaggerRouteData>
+    {
+        public TestMetadataModule()
+        {
+            Describe["Test"] = description => description.AsSwagger(with =>
+            {
+                with.ResourcePath("/test");
+                with.Summary("This is pretty");
+                with.Notes("We rock!");
+            });
+
+
+        }
+    }
+}
